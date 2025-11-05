b07313f7e741b568d789956a26dd178abc890579
bighu630 <ivhu@foxmail.com>
2025-11-05 15:35:19 +0800

    fix:adj prometheus

--- middleware/middleware_prometheus.go
+++ middleware/middleware_prometheus.go
@@ -15,16 +15,16 @@ var (
 			Name: "middleware_requests_total",
 			Help: "Total number of requests handled by middleware",
 		},
-		[]string{"client"},
+		[]string{"client", "method"},
 	)
 
 	requestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "middleware_request_duration_seconds",
 			Help:    "Histogram of request processing duration",
-			Buckets: prometheus.DefBuckets,
+			Buckets: []float64{0.1, 0.2, 0.5, 1.0, 5.0},
 		},
-		[]string{"client"},
+		[]string{"client", "method"},
 	)
 
 	requestErrors = prometheus.NewCounterVec(
@@ -32,7 +32,7 @@ var (
 			Name: "middleware_request_errors_total",
 			Help: "Total number of errors returned by handler",
 		},
-		[]string{"client"},
+		[]string{"client", "method"},
 	)
 )
 
@@ -41,31 +41,40 @@ func init() {
 	prometheus.MustRegister(requestsTotal, requestDuration, requestErrors)
 }
 
-type PrometheusClientKey struct{}
+type PrometheusClientKey struct{} // 弃用
+type PrometheusMethodKey struct{}
 
 // 从 context 获取 client label
-func GetPrometheusClientLabel(ctx context.Context, client any) string {
+func GetPrometheusClientLabel(ctx context.Context, client any) []string {
+	labels := []string{}
 	if v := ctx.Value(PrometheusClientKey{}); v != nil {
-		return fmt.Sprintf("%v", v)
+		labels = append(labels, fmt.Sprintf("%v", v))
 	}
-	return fmt.Sprintf("%v", client)
+	if v := ctx.Value(PrometheusMethodKey{}); v != nil {
+		labels = append(labels, fmt.Sprintf("%v", v))
+	}
+	return labels
 }
 
 // PrometheusMiddleware 实现
-func PrometheusMiddleware[T any]() Middleware[T] {
+func NewPrometheusMiddleware[T any]() Middleware[T] {
 	return WrapMiddleware(func(ctx context.Context, client cw.ClientWrapped[T], next func(ctx context.Context, client cw.ClientWrapped[T]) error) error {
-		// label := GetPrometheusClientLabel(ctx, client)
-		label := client.GetClientId()
+		var labels []string
+		labels = append(labels, client.GetClientId())
+		ctxLabels := GetPrometheusClientLabel(ctx, client)
+		if len(ctxLabels) > 0 {
+			labels = append(labels, ctxLabels...)
+		}
 		start := time.Now()
-		requestsTotal.WithLabelValues(label).Inc()
+		requestsTotal.WithLabelValues(labels...).Inc()
 
 		err := next(ctx, client)
 
 		duration := time.Since(start).Seconds()
-		requestDuration.WithLabelValues(label).Observe(duration)
+		requestDuration.WithLabelValues(labels...).Observe(duration)
 
 		if err != nil {
-			requestErrors.WithLabelValues(label).Inc()
+			requestErrors.WithLabelValues(labels...).Inc()
 		}
 
 		return err
