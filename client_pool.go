64e2690dcab7a821009c5ecc3cff45fd0259b2c8
bighu630 <ivhu@foxmail.com>
2026-03-19 23:44:18 +0800

    feat:add get client

--- client_pool.go
+++ client_pool.go
@@ -44,6 +44,12 @@ func NewClientPool[T any](maxFails int, cooldown time.Duration, defaultBalancer 
 	return c
 }
 
+func (c *ClientPool[T]) GetClientPool() []clientWrapper.ClientWrapped[T] {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.clients
+}
+
 // 添加client, if weight <= 0, weight = 1
 func (c *ClientPool[T]) AddClient(client T, id string, weight int) {
 	c.mu.Lock()
